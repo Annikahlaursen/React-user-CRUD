64cd249abef26f3385af315d72f4cf98a1dc965f
Annikahlaursen <Annikahlaursen@gmail.com>
2025-09-01 14:00:01 +0200

    Implement user navigation and detail fetching functionality

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,6 +1,15 @@
+import { use } from "react";
+import { useNavigate } from "react-router";
+
 export default function User({ user }) {
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate(`/users/${user.id}`);
+  }
+
   return (
-    <article className="user-card">
+    <article className="user-card" onClick={handleClick}>
       <img
         src={
           user.image || "https://placehold.co/600x400?text=Error+loading+image"
@@ -10,7 +19,7 @@ export default function User({ user }) {
       <h2>{user.name}</h2>
       <p className="title">{user.title}</p>
       <p>
-        <a href={`mailto:${user.mail}`}>{user.mail}</a>
+        <a href={`mailto:${user.email}`}>{user.mail}</a>
       </p>
     </article>
   );
--- src/pages/UserDetailPage.jsx
+++ src/pages/UserDetailPage.jsx
@@ -1,7 +1,17 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 
 export default function UpdatePage() {
   const { id } = useParams();
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const data = localStorage.getItem("users"); // get the existing users from local storage
+    const usersData = JSON.parse(data) || []; // parse the data from string to javascript array
+    const user = usersData.find((user) => user.id === id); // find the user with the matching id
+    console.log(user);
+    setUser(user); // set the user to the state
+  }, [id]); // ID VERY IMPORTANT TO HAVE IN THE DEPENDENCY ARRAY
 
   // Fetch user data based on ID
   // Example: const user = getUserById(id);
@@ -9,7 +19,7 @@ export default function UpdatePage() {
   return (
     <div id="user-page" className="page">
       <div className="container">
-        <h1>User Detail Page</h1>
+        <h1>{user.name}</h1>
         <p>User Id: {id}</p>
         {/* Display user details here */}
       </div>
